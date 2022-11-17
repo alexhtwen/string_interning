a1c296d80602a2a32040205c91fa835849210aff
Alex Van <alexhtwen@gmail.com>
2022-11-17 08:03:21 +0800

    integer interning...

--- interning.py
+++ interning.py
@@ -1,12 +1,27 @@
-import sys
+# num1 = -5    # implicit的整數interning範圍為-5 ~ 256。
+# num2 = -5
+# print(f'{num1 = }\t\t{num2 = }')
+# print(f'{id(num1) = :<20}{id(num2) = }')
+# print(f'{(id(num1) == id(num2)) = }')
 
-head = 'A'
-tail = 'lex'
-str1 = sys.intern(head + tail)
-str2 = sys.intern(head + tail)
+# print()
+# num1 = -6**99
+# num2 = -6**99
+# print(f'{num1 = }\t\t{num2 = }')
+# print(f'{id(num1) = :<20}{id(num2) = }')
+# print(f'{(id(num1) == id(num2)) = }')
 
-print(str1, str2)
-print(id(str1), id(str2))
-# print(f'{str1 = }\t\t\t{str2 = }')
-print(f'{id(str1) = :<20}{id(str2) = }')
-print(f'{(id(str1) == id(str2)) = }')
+# print()
+# num1 = 256
+# num2 = 256
+# print(f'{num1 = }\t\t{num2 = }')
+# print(f'{id(num1) = :<20}{id(num2) = }')
+# print(f'{(id(num1) == id(num2)) = }')
+
+print()
+num = 999999999999999999999999*9999999999999999999999999999999999*999999999999999999999999
+num1 = 999999999999999999999999*9999999999999999999999999999999999*999999999999999999999999
+num2 = 999999999999999999999999*9999999999999999999999999999999999*999999999999999999999999
+print(f'{num1 = }\t\t{num2 = }')
+print(f'{id(num1) = :<20}{id(num2) = }')
+print(f'{(id(num1) == id(num2)) = }')
